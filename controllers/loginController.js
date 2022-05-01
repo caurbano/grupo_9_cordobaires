e3ed7c39a8f1e8764d0ec33de67d916597e13c1d
caurbano <carla_urbano@hotmail.com>
2022-05-01 19:30:11 -0300

    para seguir con sprint 3

--- controllers/loginController.js
+++
@@ -1,8 +0,0 @@
-// const controlador {
-//     in: (req, res) => {
-//         res.sendFile(path.resolve(__dirname, './views/login.html'))
-//     }, //loguear al usuario
-//     out: //deslogueo,
-// };
-
-// module.exports = controlador;
