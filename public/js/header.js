afe996c54d95dae73f4c6b496626b45c976249df
caurbano <carla_urbano@hotmail.com>
2022-07-07 11:49:38 -0300

    js menu - search

--- public/js/header.js
+++ public/js/header.js
@@ -1,9 +1,20 @@
 window.addEventListener('load', function () {
     let menu = document.querySelector('#menu');
+    let search = document.querySelector('#search');
 
-    menu.addEventListener('click', function () {
-        // alert('prueba')
-        menu.style.color = 'Red';
+    menu.addEventListener('click', function (event){
+        let menuDropDown = document.querySelector('.menu-dd')
+
+        menuDropDown.classList.toggle('block')
+    })
+
+    search.addEventListener('click', function(event){
+        let searchForm = document.querySelector('.search_form')
+        let searchInput = document.querySelector('.input_search')
+
+        searchForm.classList.toggle('block')
+        searchInput.focus();
+
     })
 })
 
