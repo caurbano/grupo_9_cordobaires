46a9213eb5398ca2465570ba2d9e0bc2ec229989
caurbano <carla_urbano@hotmail.com>
2022-08-12 20:14:27 -0300

    carousel

---
+++ public/js/home.js
@@ -0,0 +1,20 @@
+// window.addEventListener('load', function () {
+//     let imgDest = document.querySelectorAll('img.dest');
+//     let imgDest_b = document.querySelector('img.dest_b')
+
+
+//     for (let i = 0; i < imgDest.length; i++) {
+//         imgDest[i].addEventListener('mouseover', function (event){
+//             // alert('Oh')
+//             imgDest[i].classList.toggle('dest_b')
+//             imgDest_b.classList.toggle('show')
+
+//         })
+        
+//         imgDest[i].addEventListener('mouseout', function (event){
+//             imgDest[i].classList.toggle('show')
+//             imgDest_b.classList.toggle('dest_b')
+
+//         })
+//     }
+// })
