341b45cc784742bc2cda1a47d5739ce8002d8684
caurbano <carla_urbano@hotmail.com>
2022-08-15 16:11:03 -0300

    DB - add products

--- src/database/data/data.sql
+++ src/database/data/data.sql
@@ -12,8 +12,23 @@ INSERT INTO products VALUES
 (DEFAULT, "VENTILUZ", "Base y brazo metálico. Cable alimentación 1m negro. Medidas generales: Alto 100cm x base 55cm de diámetro. Moderno diseño para 5 focos.", "pie", "bronce", 6600, 0, NOW(), NOW(), 25, 1),
 (DEFAULT, "MONROE", "Base y brazo metálico. Cable alimentación 1m negro. Medidas generales: Alto 120cm x base 45cm de diámetro. Translúcida en tonalidad gris.", "pie", "dorado", 12300, 10, NOW(), NOW(), 35, 1),
 (DEFAULT, "CILINDRO", "Aplique para iluminación de pared. Cable de alimentación interno. Medidas generales: Alto 35cm x 15cm.", "pared", "negro", 3600, 0, NOW(), NOW(), 100, 1),
-(DEFAULT, "BARRAL", "Barral de 4 s. Cable de alimentación interno. Medidas generales: Alto 34cm x 105cm.", "pared", "negro", 11400, 5, NOW(), NOW(), 50, 1),
-(DEFAULT, "SANTORINI", "Aplique simple. Cable de alimentación interno de vidrio. Base metálica cromada. Medidas generales: Alto 45cm x 25cm.", "pared", "blanco", 4230, 0, NOW(), NOW(), 100, 1);
+(DEFAULT, "BARRAL", "Barral de 4 focos. Cable de alimentación interno. Medidas generales: Alto 34cm x 105cm.", "pared", "negro", 11400, 5, NOW(), NOW(), 50, 1),
+(DEFAULT, "SANTORINI", "Aplique simple. Cable de alimentación interno. Base metálica cromada. Medidas generales: Alto 45cm x 25cm.", "pared", "blanco", 4230, 0, NOW(), NOW(), 100, 1),
+(DEFAULT, "AURORA", "Aplique triple. Cable de alimentación interno. Cable regulable hasta 50cm. Base metálica blanca. Medidas generales: Alto 25cm x 35cm.", "techo", "blanco", 44290, 0, NOW(), NOW(), 100, 1),
+(DEFAULT, "SQUARE", "Aplique para iluminación de espejo. Cable de alimentación interno. Medidas generales: Alto 55cm x 95cm.", "pared", "espejado", 30230, 5, NOW(), NOW(), 100, 1),
+(DEFAULT, "DIAMANTE", "Barral de 6 focos. Cable de alimentación interno. Base metálica cromada. Medidas generales: Alto 25cm x 125cm.", "pared", "plateado", 4230, 0, NOW(), NOW(), 100, 1),
+(DEFAULT, "LUXUS", "Araña de 7 luces. Accesorios de cristal. Cable de alimentación interno. Base metálica cromada. Medidas generales: Alto 75cm x55cm.", "techo", "plata", 52400, 5, NOW(), NOW(), 100, 1),
+(DEFAULT, "CRISTAL", "Barral de 3 focos. Cable de alimentación interno. Base metálica cromada. Medidas generales: Alto 35cm x 95cm.", "pared", "bronce", 5450, 0, NOW(), NOW(), 100, 1),
+(DEFAULT, "CIRCLES", "Aplique triple. Cable de alimentación interno. Cable color gris regulable hasta 50cm. Base metálica cromada. Medidas generales: Alto 25cm x 25cm.", "techo", "bronce", 24200, 10, NOW(), NOW(), 100, 1),
+(DEFAULT, "BUBBLES", "Barral de 6 lámparas de distintos tamaños. Cable de alimentación interno. Cables color negro regulables hasta 60cm. Base metálica cromada. Medidas lámpara más grande: Alto 23cm x 23cm.", "techo", "plateado", 48600, 15, NOW(), NOW(), 100, 1),
+(DEFAULT, "GOLDEN", "Base metálica cromada. Cuerpo con apliques de vidrio. Lámpara metálica cromada. Cable de alimentación color gris. Medidas: Alto 115cm x 40cm.", "pie", "dorado", 30800, 5, NOW(), NOW(), 100, 1),
+(DEFAULT, "RINGS", "Lámpara colgante con cadena regulable hasta 50cm. Accesorios en vidrio. Cable de alimentación interno. Base metálica cromada. Medidas lámpara con accesorios: Alto 48cm x 30cm.", "techo", "negro mate", 36600, 0, NOW(), NOW(), 100, 1),
+(DEFAULT, "CANDLES", "Lámpara colgante individual. Cable color gris regulable hasta 40cm. Cable de alimentación interno. Base metálica cromada. Medidas lámpara: Alto 28cm x 10cm.", "techo", "dorado", 3200, 0, NOW(), NOW(), 100, 1),
+(DEFAULT, "OROSS", "Lámpara led colgante con tubo metálico de 30cm. Cable de alimentación interno. Base metálica cromada. Medidas generales: Alto 68cm x 30cm.", "techo", "dorado", 14500, 5, NOW(), NOW(), 100, 1),
+(DEFAULT, "ATENAS", "Base trípode de madera y lámpara de tela. Cable de alimentación color blanco. Medidas: Alto 105cm x 45cm.", "pie", "blanco", 24600, 10, NOW(), NOW(), 100, 1),
+(DEFAULT, "VIENA", "Base de madera y lámpara de vidrio. Cable de alimentación color gris. Medidas: Alto 115cm x 25cm.", "pie", "blanco", 30200, 5, NOW(), NOW(), 100, 1),
+(DEFAULT, "OLENA", "Base de cerámica y lámpara metálica. Cable de alimentación color negro. Medidas: Alto 205cm x base 55cm de diámetro.", "pie", "negro", 13100, 15, NOW(), NOW(), 100, 1),
+(DEFAULT, "AMBAR", "Pantalla de mimbre color amarillo. Cable color negro regulable hasta 60cm. Medidas: Alto 65cm x 30cm de diámetro.", "techo", "amarillo", 10200, 5, NOW(), NOW(), 100, 1);
 -- id / name / description / category / color / price / discount / create_at / update_at / stock
 
 INSERT INTO images VALUES 
@@ -32,6 +47,21 @@ INSERT INTO images VALUES
 (DEFAULT, "product-13-CILINDRO.jpeg", 13),
 (DEFAULT, "product-14-BARRAL.jpeg", 14),
 (DEFAULT, "product-15-SANTORINI.jpeg", 15),
+(DEFAULT, "product-16-aurora.jpeg", 16),
+(DEFAULT, "product-17-square.jpeg", 17),
+(DEFAULT, "product-18-diamante.jpeg", 18),
+(DEFAULT, "product-19-luxus.jpeg", 19),
+(DEFAULT, "product-20-cristal.jpeg", 20),
+(DEFAULT, "product-21-circles.jpeg", 21),
+(DEFAULT, "product-22-bubbles.jpeg", 22),
+(DEFAULT, "product-23-golden.jpeg", 23),
+(DEFAULT, "product-24-rings.jpeg", 24),
+(DEFAULT, "product-25-candles.jpeg", 25),
+(DEFAULT, "product-26-oross.jpeg", 26),
+(DEFAULT, "product-27-atenas.jpeg", 27),
+(DEFAULT, "product-28-viena.jpeg", 28),
+(DEFAULT, "product-29-olena.jpeg", 29),
+(DEFAULT, "product-30-ambar.jpeg", 30),
 
 -- Segunda imagen de cada producto
 (DEFAULT, "product-1660352611644.jpeg", 1),
@@ -48,10 +78,26 @@ INSERT INTO images VALUES
 (DEFAULT, "product-1660355186841.jpeg", 12),
 (DEFAULT, "product-1660355352002.jpeg", 13),
 (DEFAULT, "product-1660355719788.jpeg", 14),
-(DEFAULT, "product-1660355891797.jpeg", 15);
+(DEFAULT, "product-1660355891797.jpeg", 15),
+(DEFAULT, "product-1660589148447.jpeg", 16),
+(DEFAULT, "product-1660589171618.jpeg", 17),
+(DEFAULT, "product-1660589187465.jpeg", 18),
+(DEFAULT, "product-1660589208966.jpeg", 19),
+(DEFAULT, "product-1660589229087.jpeg", 20),
+(DEFAULT, "product-1660589247644.jpeg", 21),
+(DEFAULT, "product-1660589266208.jpeg", 22),
+(DEFAULT, "product-1660589283989.jpeg", 23),
+(DEFAULT, "product-1660589300245.jpeg", 24),
+(DEFAULT, "product-1660589333995.jpeg", 25),
+(DEFAULT, "product-1660589404906.jpeg", 26),
+(DEFAULT, "product-1660589477434.jpeg", 27),
+(DEFAULT, "product-1660589538996.jpeg", 28),
+(DEFAULT, "product-1660589564653.jpeg", 29),
+(DEFAULT, "product-1660589586226.jpeg", 30);
 -- id / url / product_id
 
-INSERT INTO users VALUES (DEFAULT, "Admin", "Lumen", 1, "admin@lumenshop.com", "$2a$10$W4oZflXyNKtJ9v21yGaFiul.d3rrrM6oszY0Abp13saz1SnYsRU4G", "0303456", "user-1655677988734.png" , NOW(), NOW(), 1);
-INSERT INTO users VALUES (DEFAULT, "Usuario1", "Prueba", 0, "prueba@lumenshop.com", "$2a$10$oaltQbokfqZrSRhAMiYjbOe7HbGgQ7Asz2eDJkAOLuchpuHQyMdXC", "987654321", "default.jpg" , NOW(), NOW(), 1);
-INSERT INTO users VALUES (DEFAULT, "Usuario2", "Prueba", 0, "usuario@lumenshop.com", "$2a$10$Lqr/Wgo7c6.i1o7rgOtW5Oqsu9ONXUBoyQ2pmBtap7scmaX2.jgkq", "456789123", "default.jpg" , NOW(), NOW(), 1);
+INSERT INTO users VALUES
+(DEFAULT, "Admin", "Lumen", 1, "admin@lumenshop.com", "$2a$10$W4oZflXyNKtJ9v21yGaFiul.d3rrrM6oszY0Abp13saz1SnYsRU4G", "0303456", "user-1655677988734.png" , NOW(), NOW(), 1),
+(DEFAULT, "Usuario1", "Prueba", 0, "prueba@lumenshop.com", "$2a$10$oaltQbokfqZrSRhAMiYjbOe7HbGgQ7Asz2eDJkAOLuchpuHQyMdXC", "987654321", "default.jpg" , NOW(), NOW(), 1),
+(DEFAULT, "Usuario2", "Prueba", 0, "usuario@lumenshop.com", "$2a$10$Lqr/Wgo7c6.i1o7rgOtW5Oqsu9ONXUBoyQ2pmBtap7scmaX2.jgkq", "456789123", "default.jpg" , NOW(), NOW(), 1);
 -- id / first_name / last_name / admin / email / password / phone / img / create_at / update_at
