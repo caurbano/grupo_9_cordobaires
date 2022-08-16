41d36e96c32ae1cec65e0ec285fcc793a52d0a7b
caurbano <carla_urbano@hotmail.com>
2022-08-16 17:54:41 -0300

    DB, js, css

--- public/js/home.js
+++ public/js/home.js
@@ -1,20 +1,23 @@
 // window.addEventListener('load', function () {
 //     let imgDest = document.querySelectorAll('img.dest');
-//     let imgDest_b = document.querySelector('img.dest_b')
+//     let imgDest_b = document.querySelectorAll('img.none')
 
 
 //     for (let i = 0; i < imgDest.length; i++) {
+//         for (let j = 0; j < imgDest_b.length; j++) {
 //         imgDest[i].addEventListener('mouseover', function (event){
 //             // alert('Oh')
-//             imgDest[i].classList.toggle('dest_b')
-//             imgDest_b.classList.toggle('show')
+//             imgDest[i].classList.toggle('none')
+//             imgDest_b[j].classList.add('dest')
+//             imgDest_b[j].classList.remove('none')
 
 //         })
         
-//         imgDest[i].addEventListener('mouseout', function (event){
-//             imgDest[i].classList.toggle('show')
-//             imgDest_b.classList.toggle('dest_b')
+//         imgDest_b[j].addEventListener('mouseout', function (event){
+//             imgDest[i].classList.add('dest')
+//             imgDest_b[j].classList.add('none')
 
 //         })
+//     }
 //     }
 // })
